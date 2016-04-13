19f41a75d99cc00347b53dde226a75d1cafd90b2
rrakowski1 <robin.tselepis@gmail.com>
2016-04-13 10:57:06 -0400

    trying

--- js/main.js
+++ js/main.js
@@ -1,110 +1,47 @@
 
 //modeled off of codepen Drawing with text by Tim Holman
 
-var position = {x: 0, y: window.innerHeight/2};
-var counter = 0;
-var angleDistortion = 0;
-var images = flowerbud, flowerbud2, flowerbud3, flowerbud4;
-//probably did this wrong
-// Drawing variables
-var canvas;
-var context;
-var mouse = {x: 0, y: 0, down: false}
+$(function() {
 
-function init() {
-  canvas = document.getElementById( 'canvas' );
-  context = canvas.getContext( '2d' );
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  var currentFlower = '<div class="flower rose"></div>';
 
-  canvas.addEventListener('mousemove', mouseMove, false);
-  canvas.addEventListener('mousedown', mouseDown, false);
-  canvas.addEventListener('mouseup',   mouseUp,   false);
-  canvas.addEventListener('mouseout',  mouseUp,  false);
-  canvas.addEventListener('dblclick', doubleClick, false);
+  $("#garden").click( function(clickLocaton) {
+    var flower = $(currentFlower);
 
-  window.onresize = function(event) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
-}
+    // alert(flower);
 
-function mouseMove ( event ){
-  mouse.x = event.pageX;
-  mouse.y = event.pageY;
-  draw();
-}
+    var offset = $(this).offset();
+    var relativeX = (clickLocaton.pageX - offset.left);
+    var relativeY = (clickLocaton.pageY - offset.top);
 
-function draw() {
- if ( mouse.down ) {
-    var d = distance( position, mouse );
-    //var fontSize = minFontSize + d/2;
-    //var letter = letters[counter];
-    //var stepSize = textWidth( letter, fontSize );
+    $(flower).css({
+      top: relativeY,
+      left: relativeX
+    });
 
-    if (d > stepSize) {
-      var angle = Math.atan2(mouse.y-position.y, mouse.x-position.x);
+    $("#garden").append(flower);
+  });
 
-      //context.font = fontSize + "px Georgia";
+  ////////////////////examle///////////////////////////
+  $("#rose").click( function() {
+    currentFlower = '<div class="flower rose"></div>'
+  });
+/////////////////////////////////////////////////////////
 
-      context.save();
-      context.translate( position.x, position.y);
-      context.rotate( angle );
-      //context.fillText(letter,0,0);
-      context.restore();
+  $("#bud").click( function() {
+    currentFlower = '<div class="flower bud"></div>'
+  });
 
-      //counter++;
-      //if (counter > letters.length-1) {
-      //  counter = 0;
-      }
+  $("#tulip").click( function() {
+    currentFlower = '<div class="flower tulip"></div>'
+  });
 
-    //console.log (position.x + Math.cos( angle ) * stepSize)
-      position.x = position.x + Math.cos(angle) * stepSize;
-      position.y = position.y + Math.sin(angle) * stepSize;
+  $("#orangeflower").click( function() {
+    currentFlower = '<div class="flower orangeflower"></div>'
+  });
 
-      }
-  }
-}
+  $("#purpleflower").click( function() {
+    currentFlower = '<div class="flower purpleflower"></div>'
+  });
 
-function distance( pt, pt2 ){
-
-  var xs = 0;
-  var ys = 0;
-
-  xs = pt2.x - pt.x;
-  xs = xs * xs;
-
-  ys = pt2.y - pt.y;
-  ys = ys * ys;
-
-  return Math.sqrt( xs + ys );
-}
-
-function mouseDown( event ){
-  mouse.down = true;
-  position.x = event.pageX;
-  position.y = event.pageY;
-
-  document.getElementById('info').style.display = 'none';
-}
-
-function mouseUp( event ){
-    mouse.down = false;
-}
-
-function doubleClick( event ) {
-  canvas.width = canvas.width;
-}
-
-function textWidth( string, size ) {
-  context.font = size + "px Georgia";
-
-  if ( context.fillText ) {
-    return context.measureText( string ).width;
-  } else if ( context.mozDrawText) {
-    return context.mozMeasureText( string );
-  }
-
- };
-
-init();
+});
