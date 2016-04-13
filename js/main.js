61085ad92e88e4d82d120283ffc294db1e5909df
Eric Conrad <eric.jeffrey.conrad@gmail.com>
2016-04-13 11:58:12 -0400

    added missing semicolons to main.js

--- js/main.js
+++ js/main.js
@@ -24,24 +24,24 @@ $(function() {
 
   ////////////////////examle///////////////////////////
   $("#rose").click( function() {
-    currentFlower = '<div class="flower rose"></div>'
+    currentFlower = '<div class="flower rose"></div>';
   });
 /////////////////////////////////////////////////////////
 
   $("#bud").click( function() {
-    currentFlower = '<div class="flower bud"></div>'
+    currentFlower = '<div class="flower bud"></div>';
   });
 
   $("#tulip").click( function() {
-    currentFlower = '<div class="flower tulip"></div>'
+    currentFlower = '<div class="flower tulip"></div>';
   });
 
   $("#orangeflower").click( function() {
-    currentFlower = '<div class="flower orangeflower"></div>'
+    currentFlower = '<div class="flower orangeflower"></div>';
   });
 
   $("#purpleflower").click( function() {
-    currentFlower = '<div class="flower purpleflower"></div>'
+    currentFlower = '<div class="flower purpleflower"></div>';
   });
 
 });
